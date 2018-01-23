6657829da7c96373e5e61f7e66357d762526de80
Jeff Anderson <strat103@yahoo.com>
2018-01-22 21:28:03 -0500

    Added ABS algorithm becuase wheel slip past the peak makes the dynamics stiff, note this is able to be turned off via a tractionControlAbs flag in the varargin

--- fourwheelMatlabODE.m
+++ fourwheelMatlabODE.m
@@ -1,6 +1,8 @@
 function dx = fourwheelMatlabODE(s,x,u,sSim,auxdata,varargin)
 
-defaults = {'suppressOutput',false};
+defaults = {'suppressOutput',false
+            'tractionControlAbs',false
+            'absSlipRatioFlag',0.13};
 setDefaultsForVarargin(defaults,varargin);
 
 
@@ -13,6 +15,20 @@ input.auxdata = auxdata;
 phaseout = fourwheelContinuous(input);
 dx = phaseout.dynamics';
 
+%% Brute force ABS
+kappa_L1 = phaseout.algebraicStates.slipRatio_L1.meas;
+kappa_R1 = phaseout.algebraicStates.slipRatio_R1.meas;
+kappa_L2 = phaseout.algebraicStates.slipRatio_L2.meas;
+kappa_R2 = phaseout.algebraicStates.slipRatio_R2.meas;
+
+absFlag = false;
+if abs(kappa_L1)>absSlipRatioFlag || abs(kappa_R1)>absSlipRatioFlag || ...
+   abs(kappa_L2)>absSlipRatioFlag || abs(kappa_R2)>absSlipRatioFlag
+    input.phase.control = 0;
+    phaseout = fourwheelContinuous(input);
+    dx = phaseout.dynamics';
+    absFlag = true;
+end
 
 if ~suppressOutput
     persistent count
@@ -34,5 +50,11 @@ if ~suppressOutput
     if count == 21
         count = 1;
     end
+    
+    if absFlag
+        dataText = sprintf('%s ABS FLAG',dataText);
+    end
     fprintf('%s\n',dataText)
+    
+
 end
